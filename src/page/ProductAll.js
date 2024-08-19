0ba13b5de927d2a0ed2fa2008e37dcc518664ae4
Seungwon Park <wonvely941@gmail.com>
2024-08-19 22:06:32 +0900

    fix bug

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -11,7 +11,7 @@ const ProductAll = ({ loading, setLoading }) => {
   const getProducts = async () => {
     try {
       setLoading(true);
-      const searchQuery = query.get("q") ?? "";
+      const searchQuery = query.get("q") || "";
       const PORT_NUM = "4000";
       const url = new URL(`https://my-json-server.typicode.com/SeungW-Park/react-hnm/products?q=${searchQuery}`);
       const res = await fetch(url);
@@ -19,16 +19,16 @@ const ProductAll = ({ loading, setLoading }) => {
       const data = await res.json();
 
       // 서버측 쿼리문 미작동으로 프론트 단에서 쿼리 기능 구현
-      const filteredData = searchQuery
-        ? data.filter((item) => item.title.includes(searchQuery))
-        : data;
-      console.log(filteredData);
+      // const filteredData = searchQuery
+      //   ? data.filter((item) => item.title.includes(searchQuery))
+      //   : data;
+      // console.log(filteredData);
       setLoading(false);
-      if (filteredData.length === 0) {
-        alert("검색결과가 없습니다.\n메인페이지로 돌아갑니다.");
-        return;
-      }
-      setProductList(filteredData);
+      // if (filteredData.length === 0) {
+      //   alert("검색결과가 없습니다.\n메인페이지로 돌아갑니다.");
+      //   return;
+      // }
+      setProductList(data);
     } catch (err) {
       console.error("Error: ", err);
     }
