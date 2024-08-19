ee606935af102ef14da200974c5adb925632e7f1
Seungwon Park <wonvely941@gmail.com>
2024-08-19 22:08:34 +0900

    third commit

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -19,16 +19,16 @@ const ProductAll = ({ loading, setLoading }) => {
       const data = await res.json();
 
       // 서버측 쿼리문 미작동으로 프론트 단에서 쿼리 기능 구현
-      // const filteredData = searchQuery
-      //   ? data.filter((item) => item.title.includes(searchQuery))
-      //   : data;
-      // console.log(filteredData);
+      const filteredData = searchQuery
+        ? data.filter((item) => item.title.includes(searchQuery))
+        : data;
+      console.log('Filtered:', filteredData);
       setLoading(false);
-      // if (filteredData.length === 0) {
-      //   alert("검색결과가 없습니다.\n메인페이지로 돌아갑니다.");
-      //   return;
-      // }
-      setProductList(data);
+      if (filteredData.length === 0) {
+        alert("검색결과가 없습니다.\n메인페이지로 돌아갑니다.");
+        return;
+      }
+      setProductList(filteredData);
     } catch (err) {
       console.error("Error: ", err);
     }
